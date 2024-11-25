ba5a912639f234be1e28ea481deb50b28292d5bd
Francisco Ferreira <estudosfagf@gmail.com>
2024-11-25 16:06:12 -0300

    master

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.main_layout')
 @section('content')
 
-<p class="display-6 text-secondary text-center py-5">CONTENT</p>
+{{-- Apresenta myName a partir da Route::view --}}
+@if(!@empty($myName))
+    <p>{{$myName}}</p>
+@endif
 
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,48 @@
 <?php
 
+use App\Http\Controllers\MainController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/','home');
+//assinatura base de uma rota
+//Route::verb('uri', callback); - o callback e a ação que vai ser executada quando a rota for acionada
+
+//rota com função anônima
+Route::get('/rota', function(){
+
+     echo "<h1>Olá laravel</h1>";
+
+} );
+
+Route::get('/user', function(){
+
+    return "<h1>Aqui está o usuario!!<h1>";
+});
+
+Route::get('/injection', function(Request $request){
+
+    var_dump($request);
+});
+
+Route::match(['get', 'post'],'/match', function(){
+    return "<h1>Aceita GET e POST</h1>";
+});
+
+Route::any('/any', function(){
+    return '<h1>Aceita qualquer http verb!</h1>';
+});
+
+Route::get('/index', [MainController::class, 'index']);
+Route::get('/about', [MainController::class, 'about']);
+
+Route::redirect('/saltar', '/index');
+
+Route::permanentRedirect('/saltar2', '/about');
+
+Route::view('/view', 'home');
+
+Route::view('view2', 'home', ['myName' => "Francisco Ferreira"]);
+
+Route::fallback(function(){
+    return '404';
+});
