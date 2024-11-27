46d6d4926d91d2a23c5b8498c308c423efcc4858
Francisco Ferreira <estudosfagf@gmail.com>
2024-11-27 09:20:06 -0300

    003

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\MainController;
+use App\Http\Controllers\UserController;
+use App\Http\Middleware\OnlyAdmin;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -78,3 +80,48 @@ Route::get('/exp3/{value1}/{value2}', function($value1, $value2){
     'value1' => '[0-9]+',
     'value2' => '[A-Za-z0-9]+'
 ]);
+
+//--------------------------------------------------------------------------
+//------------------------------ROUTE NAME----------------------------------
+//--------------------------------------------------------------------------
+
+Route::get('/rota_abc', function(){
+    return 'Rota  nomeada';
+})->name('rota_nomeada');
+
+Route::get('/rota_referenciada', function(){
+    return redirect()->route('rota_nomeada');
+});
+
+Route::prefix('admin')->group(function(){
+    Route::get('/home', [MainController::class, 'index']);
+    Route::get('/about', [MainController::class, 'about']);
+    Route::get('/management', [MainController::class, 'mostrarValor']);
+
+});
+
+Route::get('/admin/only', function(){
+    echo "Apenas administradores!!";
+
+})->middleware([OnlyAdmin::class]);
+
+Route::middleware([OnlyAdmin::class])->group(function(){
+
+    Route::get('/admin/only2', function(){
+        return "Apenas administradores 1";
+    });
+    Route::get('/admin/only3', function(){
+        return "Apenas administradores 2";
+    });
+
+});
+
+Route::controller(UserController::class)->group( function(){
+    Route::get('/user/new', 'new');
+    Route::get('/user/edit', 'edit');
+    Route::get('/user/delete', 'delete');
+});
+
+Route::fallback( function(){
+    echo 'PÁGINA NÃO ENCONTRADA!';
+});
