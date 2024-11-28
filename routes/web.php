86c6d92dd980c8a8fb5ad51ed5c84a0cae82d867
Francisco Ferreira <estudosfagf@gmail.com>
2024-11-28 16:06:56 -0300

    006

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,6 @@
 <?php
 
-use App\Http\Controllers\ClientsController;
-use App\Http\Controllers\MainController;
-use App\Http\Controllers\ProductsController;
-use App\Http\Controllers\SingleActionController;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\admin\AdminController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/init', [MainController::class, 'initMethod'])->name('init');
-Route::get('/view', [MainController::class, 'viewPage'])->name('view');
-
-//Route para controller Single Action
-Route::get('/single', SingleActionController::class)->name('single');
-
-//rota para controller do tipo resource
-// Route::resource('/user', UserController::class);
-// Route::resource('/client', ClientsController::class);
-// Route::resource('/product', ProductsController::class);
-
-Route::resources([
-    'user' => UserController::class,
-    'client' => ClientsController::class ,
-    'product' => ProductsController::class
-]);
+Route::get('/admin', [AdminController::class, 'index'])->name('amdmin');
